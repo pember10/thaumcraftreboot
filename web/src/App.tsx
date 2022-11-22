480521cb1c1a6eeaf88082c104b899be7092be85
Paul Pemberton <paul.pemberton@towerswatson.com>
2022-11-22 11:36:28 -0800

    Added initial web coding

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './assets/img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming soon...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
